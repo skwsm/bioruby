ea859e8032b52b04a66185ba8c63ca8ac7259fe9
Shuichi KAWASHIMA <shuichi.kawashima@gmail.com>
2010-10-20 16:49:00 +0900

    new file: obo.rb. Just a stub class for parsing a OBO format file.

---
+++ lib/bio/db/obo.rb
@@ -0,0 +1,29 @@
+#
+# = bio/db/go.rb - Classes for Gene Ontology
+#
+# Copyright::   Copyright (C) 2010
+#               Shuichi Kawashima <shuichi.kawashima@gmail.com>
+# License::     The Ruby License
+#
+#  $Id:$
+#
+# == Gene Ontology
+#
+# == Example
+#
+# == References
+#
+
+require 'bio/pathway'
+
+module Bio
+
+# = Bio::OBO
+# Stub class for OBO format http://www.obofoundry.org/
+class OBO
+    
+end # class OBO
+
+end # module Bio
+
+
